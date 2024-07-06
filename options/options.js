1fbd29778954e2c15989bb7a8b9775bbb31ba94b
Crowdbotics <team@crowdbotics.com>
2024-07-06 22:51:00 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "the-map-48661",
+  url: "https://the-map-48661.botics.co",
+  api: "https://the-map-48661.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
